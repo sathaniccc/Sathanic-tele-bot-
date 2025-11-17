ac7897a0a83e0ca3e55feee77bfc6f9e0a1c5a35
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-11-17 20:21:29 +0530

    S A T H A N I C  -  T E L E G R A M

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import { startBot } from './src/bot.js';
 
 const app = express();
-app.get('/', (req, res) => res.send('Bot Running Successfully!'));
 
-app.listen(8000, () => console.log('🔥 Server OK on port 8000'));
+app.get('/', (req, res) => res.send("Bot Running Successfully!"));
+
+app.listen(8000, () => console.log("🔥 Server OK on port 8000"));
 
 startBot();
