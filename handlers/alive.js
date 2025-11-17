43cd8d716e394c3fd4fa6c95d7e2b03e80f30ec0
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-11-17 20:11:56 +0530

    S A T H A N I C  -  T E L E G R A M

--- handlers/alive.js
+++
@@ -1,3 +0,0 @@
-export const aliveHandler = async (bot, chatId) => {
-  bot.sendMessage(chatId, "💚 Bot Alive & Running!");
-};￼Enter
--- handlers/ping.js
+++
@@ -1,8 +0,0 @@
-export const pingHandler = async (bot, chatId) => {
-  const start = Date.now();
-  const msg = await bot.sendMessage(chatId, "📡 Checking ping...");
-  bot.editMessageText(`📡 Ping: ${Date.now() - start} ms`, {
-    chat_id: chatId,
-    message_id: msg.message_id
-  });
-};
