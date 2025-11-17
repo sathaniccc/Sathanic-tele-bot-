5ba070370adf3b6dd2dca4b44af8dcf127b51c97
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-11-17 19:49:15 +0530

    S A T H A N I C  -  T E L E G R A M

---
+++ src/menu.js
@@ -0,0 +1,12 @@
+export const mainMenu = {
+  reply_markup: {
+    inline_keyboard: [
+      [{ text: "🎬 YouTube Downloader", callback_data: "yt" }],
+      [{ text: "📱 Instagram Reel Downloader", callback_data: "ig" }],
+      [{ text: "🖼 HD Photo Enhance", callback_data: "enhance" }],
+      [{ text: "🎧 Song Downloader", callback_data: "song" }],
+      [{ text: "📡 Ping", callback_data: "ping" }],
+      [{ text: "💚 Alive Status", callback_data: "alive" }]
+    ]
+  }
+};
